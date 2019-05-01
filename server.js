84da087190e9b10ee96c6fa3b51b7f91ac8ce845
THUANLCSE <thuanluong.aidev@gmail.com>
2019-05-01 22:41:17 +0200

    show successful in client

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var redis = require('redis');
+var portRD = '6379'
+var hostRD = '172.22.209.18'
+var client = redis.createClient(portRD, hostRD); 
+var io = require('socket.io')(http)
+
+app.use('/', express.static('www'));
+
+http.listen(8000, function(){
+    console.log('listening on *:8000');
+});
+
+client.on('message', function(chan, msg) {
+	console.log(chan);
+	console.log(msg);
+  	io.sockets.emit("leapthuanio", msg);
+});
+
+client.subscribe('leapthuan');
+
+ 
