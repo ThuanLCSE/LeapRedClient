f84c401221e3a1977c7ca231d03e163148d928d2
THUANLCSE <thuanluong.aidev@gmail.com>
2019-05-24 11:21:26 +0200

    comment code

--- server.js
+++ server.js
@@ -2,23 +2,22 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var redis = require('redis');
+//Connection parameters to Redis Pub/Sub channel in server
 var portRD = '6379'
-var hostRD =  'localhost' //'172.22.209.18'
+var hostRD =  '127.0.0.1'
 var client = redis.createClient(portRD, hostRD); 
 var io = require('socket.io')(http)
-
+//static public folder which contains the html files
 app.use('/', express.static('www'));
-
+//The server runs on port 8000
 http.listen(8000, function(){
     console.log('listening on *:8000');
 });
-
+//The event listener of redis subscriber, every time the server receives a message, it sends this message to client by WebSocket protocol of Socket IO
 client.on('message', function(chan, msg) {
-	console.log(chan);
-	console.log(msg);
-  	io.sockets.emit("leapthuanio", msg);
+	//The message is sent to the broadcast leapthuanio 
+	io.sockets.emit("leapthuanio", msg);
 });
-
 client.subscribe('leapthuan');
 
  
